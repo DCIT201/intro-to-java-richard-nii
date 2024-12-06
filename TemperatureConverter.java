938974ccc218338826589c9789fd1bc27da5c399
richard-nii <richardjnr10@gmail.com>
2024-12-06 01:20:22 +0000

    Add files via upload

---
+++ TemperatureConverter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //collection of response from the user
+        System.out.println("Enter C for conversion to Celsius or F for conversion to Fahrenheit: ");
+        char response = scanner.next().charAt(0);
+
+        //checking the response of the user to either convert to celsius or fahrenheit
+        if (response == 'C') {
+            System.out.println("Enter your temperature in degrees Fahrenheit: ");
+            double fahrenheitTemperature = scanner.nextDouble();
+            double celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+            System.out.println("Your temperature in Celsius is " + celsiusTemperature);
+        }
+else if (response == 'F') {
+            System.out.println("Enter your temperature in degrees Celsius: ");
+            double celsiusTemperature = scanner.nextDouble();
+            double FahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+            System.out.println("Your temperature in Fahrenheit is " + FahrenheitTemperature);
+            System.out.println();
+        }
+    }
+}
